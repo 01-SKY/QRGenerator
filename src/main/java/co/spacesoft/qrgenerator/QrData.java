bb44e3f20554df525733a97c4772af7fa67cc99e
01-Sky <ahsun.habib97@outlook.com>
2019-01-13 14:18:40 +0600

    QR generator

---
+++ src/main/java/co/spacesoft/qrgenerator/QrData.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.spacesoft.qrgenerator;
+
+/**
+ *
+ * @author ahsun
+ */
+public class QrData {
+    public static final String CONTENT = "Name: Sharafat Ahmed\n"
+            + "Blood Group: O+ ve\n"
+            + "Designation: Senior Software Engineer & Product Manager\n"
+            + "Issued Date: 20 January, 2019\n"
+            + "Office Address: H# 562, 3rd Floor, Rd# 8, Ave# 3, Mirpur DOHS, Dhaka";
+}
---
+++ src/main/java/co/spacesoft/qrgenerator/vcard/VCardData.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.spacesoft.qrgenerator.vcard;
+
+/**
+ *
+ * @author ahsun
+ */
+public class VCardData {
+    
+    public static final String CONTENT = "BEGIN:VCARD" + System.lineSeparator()
+            + "N:Sharafat Ahmed" + System.lineSeparator()
+            + "ORG:Spacesoft" + System.lineSeparator()
+            + "TEL:01521465986" + System.lineSeparator()
+            + "URL:www.spacesoft.co" + System.lineSeparator()
+            + "EMAIL:sabir@spacesoft.co" + System.lineSeparator()
+            + "NOTE: Senior Software Engineer & Product Manager\n" + System.lineSeparator()
+            + "ADR:H# 562, 3rd Floor, Rd# 8, Ave# 3, Mirpur DOHS, Dhaka" + System.lineSeparator()
+            + "END:VCARD";
+    
+}
